0187a35ee3448c52abdf9a1c959d7a1ebae5c21a
luisguilheerme <lg.maroni@gmail.com>
2023-05-21 09:56:25 -0300

    project created

---
+++ src/main/java/com/luisguilherme/desafiodevsuperior2/DesafioDevsuperior2Application.java
@@ -0,0 +1,13 @@
+package com.luisguilherme.desafiodevsuperior2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioDevsuperior2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioDevsuperior2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/luisguilherme/desafiodevsuperior2/DesafioDevsuperior2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luisguilherme.desafiodevsuperior2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioDevsuperior2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
